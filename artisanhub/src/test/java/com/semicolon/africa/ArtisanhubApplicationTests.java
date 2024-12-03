4bd108dee3e270edec1a296b11ee050a7a8755d1
Semicolon <adedamola13@gmail.com>
2024-12-03 09:43:22 +0100

    the first commit message to this project

---
+++ artisanhub/src/test/java/com/semicolon/africa/ArtisanhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolon.africa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtisanhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
