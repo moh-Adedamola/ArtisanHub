65cfee74cc5b95105aa929edf47359ff61a8428a
Semicolon <adedamola13@gmail.com>
2024-12-03 10:16:49 +0100

    Updating changes to repository

---
+++ src/main/java/com/semicolon/artisanhub/ArtisanHubApplication.java
@@ -0,0 +1,13 @@
+package com.semicolon.artisanhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtisanHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtisanHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/semicolon/artisanhub/ArtisanHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolon.artisanhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtisanHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
